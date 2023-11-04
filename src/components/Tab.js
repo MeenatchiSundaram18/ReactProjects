53ce16e73c20c199db58939896201cebb147e259
Sundar <meenatchisundaram730@gmail.com>
2023-11-04 19:06:24 +0530

    tabs component

---
+++ src/components/Tab.js
@@ -0,0 +1,64 @@
+import { useState } from "react"
+import './tab.css';
+
+
+const Content = [
+    [
+        "React is extremely popular",
+        "It makes building complex, interactive UIs a breeze",
+        "It's powerful & flexible",
+        "It has a very active and versatile ecosystem"
+    ],
+    [
+        "Components, JSX & Props",
+        "State",
+        "Hooks (e.g., useEffect())",
+        "Dynamic rendering"
+    ],
+    [
+        "Official web page (react.dev)",
+        "Next.js (Fullstack framework)",
+        "React Native (build native mobile apps with React)"
+    ],
+];
+
+
+export default function Tabs() {
+    const [activeContentIndex, setActiveContentIndex] = useState(0);
+    return (
+        <div>
+            <header>
+                <div>
+                    <h1>React.js</h1>
+                    <p>Rendering UI Using React</p>
+                </div>
+            </header>
+            <div id="tabs">
+                <menu>
+                    <button
+                        className={activeContentIndex === 0 ? "active" : ""}
+                        onClick={() => setActiveContentIndex(0)}>
+                        Why React?
+                    </button>
+                    <button
+                        className={activeContentIndex === 1 ? "active" : ""}
+                        onClick={() => setActiveContentIndex(1)}>
+                        Core Features
+                    </button>
+                    <button
+                        className={activeContentIndex === 2 ? "active" : ""}
+                        onClick={() => setActiveContentIndex(2)}>
+                        Related Resources
+                    </button>
+                </menu>
+                <div id="tab-content">
+                    <ul>
+                        {Content[activeContentIndex].map((item) => (
+                            <li key={item}>{item}</li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
